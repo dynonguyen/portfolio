5cba84bb0730ac9884a4c4ac6771aa85757b6aaa
Tuấn Nguyễn <tuannguyentn2504@gmail.com>
2021-07-15 19:58:53 +0700

    🛠  Refactor theme setting button

--- script.js
+++ script.js
@@ -4,12 +4,17 @@ $(document).ready(function () {
 	// change theme
 	$('#themeBtn').click(function () {
 		const root = $(':root');
-		if ($(this).hasClass('fa-sun')) {
-			$(this).removeClass('fa-sun').addClass('fa-moon');
-			root.attr('data-theme', 'light');
-		} else {
-			$(this).removeClass('fa-moon').addClass('fa-sun');
+		const themeIcon = $('#themeIcon');
+		const themeWrap = $('#theme');
+
+		if (themeIcon.hasClass('fa-sun')) {
+			themeIcon.removeClass('fa-sun').addClass('fa-moon');
 			root.attr('data-theme', 'dark');
+			themeWrap.css('flex-direction', 'row-reverse');
+		} else {
+			themeIcon.removeClass('fa-moon').addClass('fa-sun');
+			root.attr('data-theme', 'light');
+			themeWrap.css('flex-direction', 'row');
 		}
 	});
 });
