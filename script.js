7549d1d8cbfb246f39570a747f85979f571b5c7c
Tuấn Nguyễn <tuannguyentn2504@gmail.com>
2021-07-16 06:34:00 +0700

    🐞 Fix get & save theme in local storage

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 /// <reference path="D:\tips\typings\jquery\globals\jquery\index.d.ts" />
 
+const LS_THEME_KEY = 'theme';
+
+// get theme in local storage
+function getTheme() {
+	const theme = localStorage.getItem(LS_THEME_KEY);
+	if (theme && theme === 'dark') {
+		$(':root').attr('data-theme', 'dark');
+	} else {
+		$(':root').attr('data-theme', 'light');
+	}
+}
+
 $(document).ready(function () {
+	// get theme
+	getTheme();
+
 	// change theme
 	$('#themeBtn').click(function () {
 		const root = $(':root');
@@ -9,12 +24,14 @@ $(document).ready(function () {
 
 		if (themeIcon.hasClass('fa-sun')) {
 			themeIcon.removeClass('fa-sun').addClass('fa-moon');
-			root.attr('data-theme', 'dark');
 			themeWrap.css('flex-direction', 'row-reverse');
+			root.attr('data-theme', 'dark');
+			localStorage.setItem(LS_THEME_KEY, 'dark');
 		} else {
 			themeIcon.removeClass('fa-moon').addClass('fa-sun');
-			root.attr('data-theme', 'light');
 			themeWrap.css('flex-direction', 'row');
+			root.attr('data-theme', 'light');
+			localStorage.setItem(LS_THEME_KEY, 'light');
 		}
 	});
 });
