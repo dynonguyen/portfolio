5465dd36f97731c71cda936c8b69696b5c2451da
Tuấn Nguyễn <tuannguyentn2504@gmail.com>
2021-07-17 06:25:11 +0700

    ➕  Add render portfolio list

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,5 +1,8 @@
 /// <reference path="D:\tips\typings\jquery\globals\jquery\index.d.ts" />
 
+const DATA_KEY = 'data-key';
+const DETAIL_KEY = 'data-id';
+
 const OPTIONS = [
 	{
 		key: 'all',
@@ -28,13 +31,106 @@ const OPTIONS = [
 	},
 ];
 
+const PROJECTS = [
+	{
+		id: '0',
+		key: 'pet-project',
+		img: 'https://picsum.photos/350/350',
+		projectName: 'Dynonary',
+		projectTech: 'ReactJS, MUI, ExpressJS, RESTFul API',
+		video: 'https://www.youtube.com/watch?v=KRMWzbuh-Vw',
+		github: 'https://github.com/TuanNguyen2504/portfolio',
+		liveDemo: 'https://dynonary.herokuapp.com',
+	},
+	{
+		id: '1',
+		key: 'pet-project',
+		img: 'https://picsum.photos/350/350',
+		projectName: 'Dynonary',
+		projectTech: 'ReactJS, MUI, ExpressJS, RESTFul API',
+		video: 'https://www.youtube.com/watch?v=KRMWzbuh-Vw',
+		github: 'https://github.com/TuanNguyen2504/portfolio',
+		liveDemo: 'https://dynonary.herokuapp.com',
+	},
+	{
+		id: '2',
+		key: 'pet-project',
+		img: 'https://picsum.photos/350/350',
+		projectName: 'Dynonary',
+		projectTech: 'ReactJS, MUI, ExpressJS, RESTFul API',
+		video: 'https://www.youtube.com/watch?v=KRMWzbuh-Vw',
+		github: 'https://github.com/TuanNguyen2504/portfolio',
+		liveDemo: 'https://dynonary.herokuapp.com',
+	},
+	{
+		id: '3',
+		key: 'pet-project',
+		img: 'https://picsum.photos/350/350',
+		projectName: 'Dynonary',
+		projectTech: 'ReactJS, MUI, ExpressJS, RESTFul API',
+		video: 'https://www.youtube.com/watch?v=KRMWzbuh-Vw',
+		github: 'https://github.com/TuanNguyen2504/portfolio',
+		liveDemo: 'https://dynonary.herokuapp.com',
+	},
+	{
+		id: '3',
+		key: 'learning',
+		img: 'https://picsum.photos/350/350',
+		projectName: 'Dynonary',
+		projectTech: 'ReactJS, MUI, ExpressJS, RESTFul API',
+		video: null,
+		github: 'https://github.com/TuanNguyen2504/portfolio',
+		liveDemo: 'https://dynonary.herokuapp.com',
+	},
+	{
+		id: '4',
+		key: 'learning',
+		img: 'https://picsum.photos/350/350',
+		projectName: 'Dynonary',
+		projectTech: 'ReactJS, MUI, ExpressJS, RESTFul API',
+		video: null,
+		github: 'https://github.com/TuanNguyen2504/portfolio',
+		liveDemo: null,
+	},
+	{
+		id: '5',
+		key: 'sharing',
+		img: 'https://picsum.photos/350/350',
+		projectName: 'Dynonary',
+		projectTech: 'ReactJS, MUI, ExpressJS, RESTFul API',
+		video: '',
+		github: 'https://github.com/TuanNguyen2504/portfolio',
+		liveDemo: null,
+	},
+	{
+		id: '6',
+		key: 'others',
+		img: 'https://picsum.photos/350/350',
+		projectName: 'Dynonary',
+		projectTech: 'ReactJS, MUI, ExpressJS, RESTFul API',
+		video: '',
+		github: 'https://github.com/TuanNguyen2504/portfolio',
+		liveDemo: null,
+	},
+	{
+		id: '7',
+		key: 'others',
+		img: 'https://picsum.photos/350/350',
+		projectName: 'Dynonary',
+		projectTech: 'ReactJS, MUI, ExpressJS, RESTFul API',
+		video: '',
+		github: 'https://github.com/TuanNguyen2504/portfolio',
+		liveDemo: null,
+	},
+];
+
 let currentOptionKey = OPTIONS[0].key;
 
 function renderOptions() {
 	let xml = '';
 
 	OPTIONS.forEach((item) => {
-		xml += `<li data-key="${item.key}" class="portfolio-menu-option ${
+		xml += `<li ${DATA_KEY}="${item.key}" class="portfolio-menu-option ${
 			item.key === currentOptionKey ? 'active' : ''
 		}">${item.label}</li>`;
 	});
@@ -42,10 +138,58 @@ function renderOptions() {
 	$('#portfolioMenu').html(xml);
 }
 
+function renderProject(projects = []) {
+	let xml = '';
+	projects.forEach((project) => {
+		const { id, key, img, projectName, projectTech, video, github, liveDemo } =
+			project;
+
+		xml += `<li class="portfolio-item">
+	<img src="${img}" alt="Portfolio Photo" />
+	<div class="portfolio-item-detail">
+		<h2 class="project-name">${projectName}</h2>
+		<p class="project-tech">${projectTech}</p>
+		<div class="d-flex">
+			<a class="cur-pointer" ${DETAIL_KEY}="${id}">
+				<i class="fas fa-info-circle project-icon"></i>
+			</a>
+			${
+				video && video !== ''
+					? `<a href="${video}" target="_blank">
+							<i	i class="fas fa-play-circle project-icon"></i>
+						</a>`
+					: ''
+			}
+			${
+				github && github !== ''
+					? `	<a href="${github}" target="_blank">
+								<i class="fab fa-github project-icon"></i>
+						</a>`
+					: ''
+			}
+		${
+			liveDemo && liveDemo !== ''
+				? `<a href="${liveDemo}" target="_blank">
+					<i class="fas fa-eye project-icon"></i>
+				</a>`
+				: ''
+		}
+			
+		</div>
+	</div>
+</li>`;
+	});
+
+	$('#portfolioList').fadeOut().html(xml).fadeIn(350);
+}
+
 $(document).ready(function () {
 	renderOptions();
 
-	// option click
+	// render init all projects
+	renderProject(PROJECTS);
+
+	// filter project
 	$('.portfolio-menu-option').click(function () {
 		if ($(this).hasClass('active')) {
 			return;
@@ -58,7 +202,14 @@ $(document).ready(function () {
 
 		// change menu title
 		const newLabel = OPTIONS.find((i) => i.key === currentOptionKey).title;
-
 		$('#portfolioMenuLabel').text(newLabel);
+
+		// filter
+		if (currentOptionKey === 'all') {
+			renderProject(PROJECTS);
+		} else {
+			const filtered = PROJECTS.filter((i) => i.key === currentOptionKey);
+			renderProject(filtered);
+		}
 	});
 });
